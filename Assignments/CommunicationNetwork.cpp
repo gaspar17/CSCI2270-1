38195208eb5239d75a444ab4e919a5b064ac128e
Evan Spruston <espruston@gmail.com>
2018-02-17 21:34:42 -0700

    renamed files to avoid errors. Work as of 9:34 2/17 on assignment 4

---
+++ Assignments/CommunicationNetwork.cpp
@@ -0,0 +1,141 @@
+//Assignment 4
+//Evan Spruston
+//Hoenigman 2270
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include "CommunicationNetwork.h"
+using namespace std;
+
+CommunicationNetwork::CommunicationNetwork(){
+
+}
+
+CommunicationNetwork::~CommunicationNetwork(){
+
+}
+
+void CommunicationNetwork::addCity(std::string newCityName, std::string previousCityName){
+  if(previousCityName == "First"){
+    City *newCity = new City(newCityName, head, nullptr, " ");
+    head = newCity;
+  }
+  else if(previousCityName == "" || previousCityName == tail -> cityName){
+    City *newCity = new City(newCityName, nullptr, tail, " ");
+    tail = newCity;
+  }
+  else{
+    City *temp = head;
+    while(temp -> cityName != previousCityName){
+      if(temp -> next == nullptr){
+          cout << temp -> cityName << endl;
+          cout << "No matching city name found" << endl;
+          break;
+      }
+      temp = temp -> next;
+    }
+    City *newCity = new City(newCityName, temp -> next, temp, " ");
+    temp -> next -> previous = newCity;
+    temp -> next = newCity;
+  }
+}
+
+void CommunicationNetwork::printNetwork(){
+  City *temp = head;
+  cout << "===CURRENT PATH===" << endl << "NULL <- " << endl;
+  while(temp -> next != nullptr){
+    cout << temp -> cityName << " <-> ";
+    temp = temp -> next;
+  }
+  cout << temp -> cityName << " -> NULL" << endl << "==================" << endl;
+}
+
+void CommunicationNetwork::buildNetwork(){
+
+  City *head = new City(" ", nullptr, nullptr, " ");
+  City *Bos = new City("Boston", nullptr, nullptr, " ");
+  City *NY = new City("New York", Bos, nullptr, " ");
+  City *WashDC = new City("Washington, D.C.", NY, nullptr, " ");
+  City *Atl = new City("Atlanta", WashDC, nullptr, " ");
+  City *Chi = new City("Chicago", Atl, nullptr, " ");
+  City *StL = new City("St. Louis", Chi, nullptr, " ");
+  City *Dal = new City("Dallas", StL, nullptr, " ");
+  City *Den = new City("Denver", Dal, nullptr, " ");
+  City *Pho = new City("Phoenix", Den, nullptr, " ");
+  City *LA = new City("Los Angeles", Pho, nullptr, " ");
+
+  Pho -> previous = LA;
+  Den -> previous = Pho;
+  Dal -> previous = Den;
+  StL -> previous = Dal;
+  Chi -> previous = StL;
+  Atl -> previous = Chi;
+  WashDC -> previous = Atl;
+  NY -> previous = WashDC;
+  Bos -> previous = NY;
+
+  head = LA;
+  tail = Bos;
+
+  CommunicationNetwork::printNetwork();
+
+}
+
+void CommunicationNetwork::transmitMsg(char* filename){
+  if(head == nullptr){
+    cout << "Empty list" << endl;
+    return;
+  }
+  City *temp  = head;
+
+  ifstream messageStream;
+  string line;
+  string word;
+  messageStream.open(filename);
+
+  if(messageStream.is_open()){
+    getline(messageStream, line);
+    stringstream ss(line);
+    while(getline(ss, word, ' ')){
+      while(temp -> next != nullptr){
+        temp -> message = word;
+        cout << temp -> cityName << " received " << temp -> message << endl;
+        temp -> message = " ";
+        temp = temp -> next;
+      }
+      temp -> message = word;
+      cout << temp -> cityName << " received " << temp -> message << endl;
+      temp = head;
+    }
+  }
+  else{
+    cout << "Error reading message file" << endl;
+  }
+}
+
+void CommunicationNetwork::deleteCity(std::string removeCity){
+  City *temp = head;
+  while(temp -> cityName != removeCity){
+    if(temp -> next == nullptr){
+        cout << temp -> cityName << endl;
+        cout << "No matching city name found" << endl;
+        break;
+    }
+    temp = temp -> next;
+  }
+  temp -> previous -> next = temp -> next;
+  temp -> next -> previous = temp -> previous;
+  delete[] temp;
+}
+
+void CommunicationNetwork::deleteNetwork(){
+  City *temp = head -> next;
+  while(temp != nullptr){
+    delete[] temp -> previous;
+    temp = temp -> next;
+  }
+  delete[] tail;
+  head = NULL;
+}
--- Assignments/assignment4commNetMain.cpp
+++ Assignments/assignment4commNetMain.cpp
@@ -1,17 +1,16 @@
 #include <iostream>
-#include "assignment4commNet.cpp"
+#include "CommunicationNetwork.cpp"
 using namespace std;
 
 int main(){
 
-  assignment4commNet a(10);
+  CommunicationNetwork a;
   City *head = new City(" ", nullptr, nullptr, " ");
   int userChoice;
 
   while(userChoice != 7){
 
     int userChoice;
-    cin >> userChoice;
 
     cout << "======Main Menu======" << endl;
     cout << "1. Build Network" << endl;
@@ -22,27 +21,39 @@ int main(){
     cout << "6. Clear Network" << endl;
     cout << "7. Quit" << endl;
 
-    if(userChoice == 7){
+    cin >> userChoice;
+
+    if(userChoice == 7){ //Quit
       cout << "Goodbye!" << endl;
       return 0;
     }
-    else if(userChoice == 1){
+    else if(userChoice == 1){ //Build Network (default)
       a.buildNetwork();
     }
-    else if(userChoice == 2){
+    else if(userChoice == 2){ //Print path
       a.printNetwork();
     }
-    else if(userChoice == 3){
-      a.transmitMsg("messagein.txt");
+    else if(userChoice == 3){ //Transmit
+      char *filename = (char*)"messageIn.txt";
+      a.transmitMsg(filename);
     }
-    else if(userChoice == 4){
-
+    else if(userChoice == 4){ //Add city
+      string newCityName;
+      string previousCityName;
+      cout << "Enter a city name:" << endl;
+      cin >> newCityName;
+      cout << "Enter a previous city name:" << endl;
+      cin >> previousCityName;
+      a.addCity(newCityName, previousCityName);
     }
-    else if(userChoice == 5){
-
+    else if(userChoice == 5){ //Delete city
+      string removeCity;
+      cout << "Enter a city name:" << endl;
+      cin >> removeCity;
+      a.deleteCity(removeCity);
     }
-    else if(userChoice == 6){
-
+    else if(userChoice == 6){ //Clear network
+      a.deleteNetwork();
     }
     else{
       cout << "Error, input not valid, please enter a number 1-7" << endl;
